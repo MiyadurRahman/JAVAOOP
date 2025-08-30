8ff5b99312154cae2bb9d97e1aa4b847a5d3e26b
Meyadur Rahman <meyadurrahman777@gmail.com>
2025-08-30 22:19:35 +0600

    java pogramming

---
+++ src/Inheritance/mouse.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+public class mouse {
+
+   public static void leftclick(){
+       System.out.println("click");
+
+   }
+    public static void rightclick(){
+        System.out.println("click");
+
+    }
+    public static void scrollup(){
+        System.out.println("up");
+
+    }
+    public static void scrolldown(){
+        System.out.println("down");
+
+    }
+}
